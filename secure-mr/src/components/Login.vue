d1d279fd9f36e67f377bfc696a9b50d0e215d3ca
anagparedes <anagparedesg@gmail.com>
2021-12-07 07:31:37 -0400

    [ADD] Login Component

---
+++ secure-mr/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+<img class="logo" src="../assets/secure-mr-logo.png"/>
+<h1>Login</h1>
+<div class="login">
+<input type="text" v-model="email" placeholder="Tú correo electrónico"/>
+<button v-on:click="loginPassword">Iniciar Sesión con Contraseña</button>
+<button v-on:click="loginBiometry">Iniciar Sesión con Biometría</button>
+<button v-on:click="login">Login</button>
+<p>
+<router-link to="Home">Login</router-link>
+</p>
+</div>
+</template>
+<script>
+export default{
+name: 'Login'
+}
+</script>
