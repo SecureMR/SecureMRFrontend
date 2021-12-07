6b620f7e8a828acb03e61ab06a932895d94f1855
anagparedes <anagparedesg@gmail.com>
2021-12-07 07:51:10 -0400

    [FIX] login component

--- secure-mr/src/components/Login.vue
+++ secure-mr/src/components/Login.vue
@@ -1,18 +1,49 @@
 <template>
-<img class="logo" src="../assets/secure-mr-logo.png"/>
-<h1>Login</h1>
-<div class="login">
-<input type="text" v-model="email" placeholder="Tú correo electrónico"/>
-<button v-on:click="loginPassword">Iniciar Sesión con Contraseña</button>
-<button v-on:click="loginBiometry">Iniciar Sesión con Biometría</button>
-<button v-on:click="login">Login</button>
-<p>
-<router-link to="Home">Login</router-link>
-</p>
-</div>
+<img>
+    <div id="login">
+    <img src="secure-mr.logo" alt="Logo" width="500" height="600">
+        <h1>Login</h1>
+        <input type="text" name="username" v-model="input.username" placeholder="Username" />
+        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+        <button type="button" v-on:click="login()">Login</button>
+    </div>
 </template>
+
 <script>
-export default{
-name: 'Login'
-}
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
 </script>
+
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+</style>
